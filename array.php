6a7a92acfec8fe8197622d3de351d56753178d00
Andi <Andigunaputra7@gmail.com>
2026-03-11 15:09:04 +0700

    first commit

---
+++ array.php
@@ -0,0 +1,7 @@
+<?php
+$buah = array("Apel","Mangga","Jeruk");
+
+foreach ($buah as $b){
+    echo $b . "<br>";
+}
+?>
---
+++ variabel.php
@@ -0,0 +1,8 @@
+<?php
+$nama = "Andi";
+$umur = 20;
+$nilai = 85.5;
+$status = true;
+
+echo $nama;
+?>
